03dd8744cb6c4a76b04ff65b1b243c222924b9b1
rmadamson <rmadamson@gmail.com>
2017-01-13 21:33:42 -0500

    Moved all timekeeping calculations to sethand function
    We were essentially duplicating code for hours, minutes,
    and seconds, so I moved all that logic into a sethand
    function.  Quite nice.

--- Watch_Clock_Display/Watch_Clock_Display.ino
+++ Watch_Clock_Display/Watch_Clock_Display.ino
@@ -2,7 +2,7 @@
 
 #define PIN 12
 #define NUMPIXELS 12
-#define BRIGHTNESS 20
+#define BRIGHTNESS 30
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -13,11 +13,9 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  //while(!Serial);
-    //delay(500);
-
+//  while(!Serial);
+//    delay(500);
 //  Serial.begin(9600);
 //  Serial.println(F("Started"));
 
@@ -30,63 +28,39 @@ void loop() {
 
 void displaytime( uint32_t t ) {
 
-  uint8_t color[NUMPIXELS][3];
-  memset( color, 0, sizeof(color) );
+  uint8_t red[NUMPIXELS];
+  uint8_t green[NUMPIXELS];
+  uint8_t blue[NUMPIXELS];
+  
+  memset( red, 0, sizeof(red) );
+  memset( green, 0, sizeof(green) );
+  memset( blue, 0, sizeof(blue) );
   
   uint16_t seconds = t % 60;
   uint16_t minutes = t % (60 * 60);
   uint16_t hours   = t % (60 * 60 * 12);
 
   uint16_t numbuckets  = NUMPIXELS;
-  uint16_t bucketwidth = 60 / numbuckets;
-
-  // Let's figure out seconds
-  uint16_t secondsbucket = seconds / bucketwidth;
-  float secondspower = float(seconds - (secondsbucket * bucketwidth)) / float(bucketwidth);
   
-  color[secondsbucket % numbuckets][2] = float(BRIGHTNESS) * (1-(secondspower*secondspower*secondspower));
-  color[(secondsbucket + 1) % numbuckets][2] = float(BRIGHTNESS) * secondspower*secondspower*secondspower;
+  sethand( red,   hours,   numbuckets, 60*60*12/numbuckets );
+  sethand( green, minutes, numbuckets, 60*60/numbuckets );
+  sethand( blue,  seconds, numbuckets, 60/numbuckets );
 
-
-  bucketwidth = 60*60 / numbuckets;
-  uint16_t minutesbucket = minutes / bucketwidth;
-  float minutespower = float(minutes - (minutesbucket * bucketwidth)) / float(bucketwidth);
-  
-  color[minutesbucket][1] = float(BRIGHTNESS) * (1-(minutespower*minutespower*minutespower));
-  color[(minutesbucket + 1) % numbuckets][1] = float(BRIGHTNESS) * minutespower*minutespower*minutespower;
-
-
-  bucketwidth = 60*60*12 / numbuckets;
-  uint16_t hoursbucket = hours / bucketwidth;
-  float hourspower = float(hours - (hoursbucket * bucketwidth)) / float(bucketwidth);
-  
-  color[hoursbucket][0] = float(BRIGHTNESS) * (1-(hourspower*hourspower*hourspower));
-  color[(hoursbucket + 1) % numbuckets][0] = float(BRIGHTNESS) * hourspower*hourspower*hourspower;
-
- // Serial.print(F("Time: "));
- // Serial.println(t);
- // Serial.println(minutespower);
- // Serial.println((1-(minutespower*minutespower*minutespower)));
- // Serial.println(((minutespower*minutespower*minutespower)));
-
-
-//  Serial.print(F("Seconds:        "));
-//  Serial.println(seconds);
-//  Serial.print(F("Bucketwidth:    "));
-//  Serial.println(bucketwidth);
-  
-//  Serial.print(F("Secondsbucket:  "));
-//  Serial.println(secondsbucket);
-
-//  Serial.print(F("Secondspower:   "));
-//  Serial.println(secondspower);
-  
-
-  
   for( uint16_t i=0; i < numbuckets; i++ ) {
-    strip.setPixelColor(i, strip.Color(color[i][0], color[i][1], color[i][2]));
+    strip.setPixelColor(i, strip.Color(red[i], green[i], blue[i]));
   }
   
   strip.show();
-  
 }
+
+
+void sethand( uint8_t *color, uint16_t units, uint16_t numbuckets, uint16_t bucketwidth ) {
+
+  uint16_t bucket = units / bucketwidth;
+  float power = float(units - (bucket * bucketwidth)) / float(bucketwidth);
+  
+  color[bucket % numbuckets] = float(BRIGHTNESS) * (1-(power*power));
+  color[(bucket + 1) % numbuckets] = float(BRIGHTNESS)*power*power;
+
+}
+
